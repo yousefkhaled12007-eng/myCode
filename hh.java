f9ce33ccdcc16aa4f12c9315ca2b45c45e8386fc
Yousef <yousefkhaled1.2007@gmail.com>
2026-03-04 02:47:21 +0200

    Add files via upload

---
+++ hh.java
@@ -0,0 +1,22 @@
+import java.util.Scanner ;
+public class hh {
+
+
+public static void main (String[] args) {
+Scanner input = new Scanner(System.in) ;
+System.out.println(" enter the first num ");
+int num1 = input.nextInt() ;
+System.out.println(" ok enter the second ");
+int num2 = input.nextInt() ;
+System.out.println("this is the sum" + num1 + num2 );
+System.out.println("this is the minus " + (num1 - num2));
+System.out.println("this is the " + num1 / num2);
+System.out.println("this is the " + num1 * num2);
+
+
+
+}
+
+
+}
+    
